7215ee5c074a1d026de560b75ceea236217031e1
Colin Ross <colinross@users.noreply.github.com>
2018-09-06 14:01:23 -0700

    Enable logidze

--- db/structure.sql
+++ db/structure.sql
@@ -23,6 +23,20 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 --
+-- Name: hstore; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION hstore; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION hstore IS 'data type for storing sets of (key, value) pairs';
+
+
+--
 -- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -48,6 +62,223 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 --
 
 COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+--
+-- Name: logidze_compact_history(jsonb); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.logidze_compact_history(log_data jsonb) RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE
+          merged jsonb;
+        BEGIN
+          merged := jsonb_build_object(
+            'ts',
+            log_data#>'{h,1,ts}',
+            'v',
+            log_data#>'{h,1,v}',
+            'c',
+            (log_data#>'{h,0,c}') || (log_data#>'{h,1,c}')
+          );
+
+          IF (log_data#>'{h,1}' ? 'm') THEN
+            merged := jsonb_set(merged, ARRAY['m'], log_data#>'{h,1,m}');
+          END IF;
+
+          return jsonb_set(
+            log_data,
+            '{h}',
+            jsonb_set(
+              log_data->'h',
+              '{1}',
+              merged
+            ) - 0
+          );
+        END;
+      $$;
+
+
+--
+-- Name: logidze_exclude_keys(jsonb, text[]); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.logidze_exclude_keys(obj jsonb, VARIADIC keys text[]) RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE
+          res jsonb;
+          key text;
+        BEGIN
+          res := obj;
+          FOREACH key IN ARRAY keys
+          LOOP
+            res := res - key;
+          END LOOP;
+          RETURN res;
+        END;
+      $$;
+
+
+--
+-- Name: logidze_logger(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.logidze_logger() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE
+          changes jsonb;
+          version jsonb;
+          snapshot jsonb;
+          new_v integer;
+          size integer;
+          history_limit integer;
+          current_version integer;
+          merged jsonb;
+          iterator integer;
+          item record;
+          columns_blacklist text[];
+          ts timestamp with time zone;
+          ts_column text;
+        BEGIN
+          ts_column := NULLIF(TG_ARGV[1], 'null');
+          columns_blacklist := TG_ARGV[2];
+
+          IF TG_OP = 'INSERT' THEN
+            snapshot = logidze_snapshot(to_jsonb(NEW.*), ts_column, columns_blacklist);
+
+            IF snapshot#>>'{h, -1, c}' != '{}' THEN
+              NEW.log_data := snapshot;
+            END IF;
+
+          ELSIF TG_OP = 'UPDATE' THEN
+
+            IF OLD.log_data is NULL OR OLD.log_data = '{}'::jsonb THEN
+              snapshot = logidze_snapshot(to_jsonb(NEW.*), ts_column, columns_blacklist);
+              IF snapshot#>>'{h, -1, c}' != '{}' THEN
+                NEW.log_data := snapshot;
+              END IF;
+              RETURN NEW;
+            END IF;
+
+            history_limit := NULLIF(TG_ARGV[0], 'null');
+            current_version := (NEW.log_data->>'v')::int;
+
+            IF ts_column IS NULL THEN
+              ts := statement_timestamp();
+            ELSE
+              ts := (to_jsonb(NEW.*)->>ts_column)::timestamp with time zone;
+              IF ts IS NULL OR ts = (to_jsonb(OLD.*)->>ts_column)::timestamp with time zone THEN
+                ts := statement_timestamp();
+              END IF;
+            END IF;
+
+            IF NEW = OLD THEN
+              RETURN NEW;
+            END IF;
+
+            IF current_version < (NEW.log_data#>>'{h,-1,v}')::int THEN
+              iterator := 0;
+              FOR item in SELECT * FROM jsonb_array_elements(NEW.log_data->'h')
+              LOOP
+                IF (item.value->>'v')::int > current_version THEN
+                  NEW.log_data := jsonb_set(
+                    NEW.log_data,
+                    '{h}',
+                    (NEW.log_data->'h') - iterator
+                  );
+                END IF;
+                iterator := iterator + 1;
+              END LOOP;
+            END IF;
+
+            changes := hstore_to_jsonb_loose(
+              hstore(NEW.*) - hstore(OLD.*)
+            );
+
+            new_v := (NEW.log_data#>>'{h,-1,v}')::int + 1;
+
+            size := jsonb_array_length(NEW.log_data->'h');
+            version := logidze_version(new_v, changes, ts, columns_blacklist);
+
+            IF version->>'c' = '{}' THEN
+              RETURN NEW;
+            END IF;
+
+            NEW.log_data := jsonb_set(
+              NEW.log_data,
+              ARRAY['h', size::text],
+              version,
+              true
+            );
+
+            NEW.log_data := jsonb_set(
+              NEW.log_data,
+              '{v}',
+              to_jsonb(new_v)
+            );
+
+            IF history_limit IS NOT NULL AND history_limit = size THEN
+              NEW.log_data := logidze_compact_history(NEW.log_data);
+            END IF;
+          END IF;
+
+          return NEW;
+        END;
+        $$;
+
+
+--
+-- Name: logidze_snapshot(jsonb, text, text[]); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.logidze_snapshot(item jsonb, ts_column text, blacklist text[] DEFAULT '{}'::text[]) RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE
+          ts timestamp with time zone;
+        BEGIN
+          IF ts_column IS NULL THEN
+            ts := statement_timestamp();
+          ELSE
+            ts := coalesce((item->>ts_column)::timestamp with time zone, statement_timestamp());
+          END IF;
+          return json_build_object(
+            'v', 1,
+            'h', jsonb_build_array(
+                   logidze_version(1, item, ts, blacklist)
+                 )
+            );
+        END;
+      $$;
+
+
+--
+-- Name: logidze_version(bigint, jsonb, timestamp with time zone, text[]); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.logidze_version(v bigint, data jsonb, ts timestamp with time zone, blacklist text[] DEFAULT '{}'::text[]) RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE
+          buf jsonb;
+        BEGIN
+          buf := jsonb_build_object(
+                   'ts',
+                   (extract(epoch from ts) * 1000)::bigint,
+                   'v',
+                    v,
+                    'c',
+                    logidze_exclude_keys(data, VARIADIC array_append(blacklist, 'log_data'))
+                   );
+          IF coalesce(current_setting('logidze.meta', true), '') <> '' THEN
+            buf := jsonb_set(buf, ARRAY['m'], current_setting('logidze.meta')::jsonb);
+          END IF;
+          RETURN buf;
+        END;
+      $$;
 
 
 SET default_tablespace = '';
@@ -120,6 +351,8 @@ SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20180906202100'),
-('20180906204643');
+('20180906204643'),
+('20180906210034'),
+('20180906210035');
 
 
